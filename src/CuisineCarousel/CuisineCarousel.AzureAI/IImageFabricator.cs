40757b0d647b95ec680334530950b366a893aa72
Matthijs van der Veer <subscriptions@vanderveer.io>
2024-11-10 09:14:24 +0100

    ✨ Adding code

---
+++ src/CuisineCarousel/CuisineCarousel.AzureAI/IImageFabricator.cs
@@ -0,0 +1,18 @@
+﻿using CuisineCarousel.Models;
+using Microsoft.SemanticKernel.TextToImage;
+
+namespace CuisineCarousel.AzureAI;
+
+internal sealed class ImageFabricator(ITextToImageService textToImageService) : IFabricator
+{
+    public async Task Fabricate(Recipe recipe)
+    {
+        // step 1 is to create the prompt with our new prompt creation function
+        var prompt = "A picture of deep-fried pasta carbonara, served in a wicker basket. In the style of 1950 sci-fi retro comic book.";
+
+        // step 2 is to gen the image with that prompt
+        var image = await textToImageService.GenerateImageAsync(prompt, 1024, 1024);
+
+        return;
+    }
+}
---
+++ src/CuisineCarousel/CuisineCarousel/IFabricator.cs
@@ -0,0 +1,8 @@
+﻿using CuisineCarousel.Models;
+
+namespace CuisineCarousel;
+
+public interface IFabricator
+{
+    Task Fabricate(Recipe recipe);
+}
