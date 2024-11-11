98f0b400e38d97e3e9086a5708ad5abd407f9c64
Matthijs van der Veer <736727+MatthijsvdVeer@users.noreply.github.com>
2024-11-11 14:54:07 +0100

    ✨ Switch to GPT-4o-mini

--- src/CuisineCarousel/CuisineCarousel.AzureAI/ModelNames.cs
+++ src/CuisineCarousel/CuisineCarousel.AzureAI/ModelNames.cs
@@ -4,4 +4,6 @@ internal static class ModelNames
 {
     public const string Gpt35 = "gpt-35-turbo-16k-default";
     public const string Gpt4o = "gpt-4o";
+    public const string Gpt4oMini = "gpt-4o-mini";
+    public const string DallE3 = "dall-e-3";
 }
