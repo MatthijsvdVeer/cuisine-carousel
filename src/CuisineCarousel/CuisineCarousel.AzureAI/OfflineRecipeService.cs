42d466e4317e375df4984bba948887f057f2cbc8
Matthijs van der Veer <736727+MatthijsvdVeer@users.noreply.github.com>
2024-11-12 11:46:07 +0100

    ♻️ Splitting into components

--- src/CuisineCarousel/CuisineCarousel.AzureAI/OfflineRecipeService.cs
+++ src/CuisineCarousel/CuisineCarousel.AzureAI/OfflineRecipeService.cs
@@ -4,17 +4,23 @@ namespace CuisineCarousel.AzureAI;
 
 internal sealed class OfflineRecipeService : IRecipe
 {
+    private static readonly Recipe recipe = new Recipe("Pad Thai Eiffel Tower", "A twist on the classic Pad Thai, inspired by the Eiffel Tower", "1. Boil water. 2. Add noodles. 3. Stir in sauce. 4. Garnish with Eiffel Tower-shaped carrots.");
+
     public Task<Recipe> CreateRecipeAsync(string originalDishName, string originalDishDescription, string twist)
     {
         return Task.FromResult(
-            new Recipe("Pad Thai Eiffel Tower", "A twist on the classic Pad Thai, inspired by the Eiffel Tower", "1. Boil water. 2. Add noodles. 3. Stir in sauce. 4. Garnish with Eiffel Tower-shaped carrots."));
+            recipe);
     }
 
     public async IAsyncEnumerable<CollaborationStep> CollaborateOnRecipeAsync(string originalDishName, string originalDishDescription, string twist)
     {
         var steps = new List<CollaborationStep>
         {
-            new("CreateRecipe", null, new("Pad Thai Eiffel Tower", "A twist on the classic Pad Thai, inspired by the Eiffel Tower", "1. Boil water. 2. Add noodles. 3. Stir in sauce. 4. Garnish with Eiffel Tower-shaped carrots.")),
+            new("CreateRecipe", null, recipe),
+            new("GordonRamsAI", "You donkey!", null),
+            new("CreateRecipe", null, recipe),
+            new("GordonRamsAI", "You donkey!", null),
+            new("CreateRecipe", null, recipe),
             new("GordonRamsAI", "You donkey!", null),
         };
 
